9500109f1b9998d24e22536d14678b08603f8865
Maris <macsantos@sga.pucminas.br>
2024-02-28 08:55:56 -0300

    Requisitos 2 e 4

---
+++ lab01/sprint01/req02.py
@@ -0,0 +1,13 @@
+from github_graph_ql import run_query
+
+def get_repositories_req2(cursor=None, num_repos=100, query_type="stars:>0", type="REPOSITORY", pages=1):
+    query = f'search(query: "{query_type}", type: {type}, first: {num_repos})'
+    query += '{ nodes { ... on Repository { name pullRequests(states: MERGED) { totalCount } } } }'
+    result = run_query(query)
+    return result
+
+
+
+if __name__ == '__main__':
+   repositories =  get_repositories_req2(num_repos=100, pages=1)
+   print(repositories)
