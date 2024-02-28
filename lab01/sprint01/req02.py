c8df75a63f28b7924b4a94628d5648410d969849
HugoPoletto34 <hugopoletto34@gmail.com>
2024-02-28 11:18:47 -0300

    Correção query 2 e 4

--- lab01/sprint01/req02.py
+++ lab01/sprint01/req02.py
@@ -1,8 +1,8 @@
 from github_graph_ql import run_query
 
 def get_repositories_req2(cursor=None, num_repos=100, query_type="stars:>0", type="REPOSITORY", pages=1):
-    query = f'search(query: "{query_type}", type: {type}, first: {num_repos})'
-    query += '{ nodes { ... on Repository { name pullRequests(states: MERGED) { totalCount } } } }'
+    query = '{ search(query: "%s", type: %s, first: %s)' % (query_type, type, num_repos)
+    query += '{ nodes { ... on Repository { name pullRequests(states: MERGED) { totalCount } } } } }'
     result = run_query(query)
     return result
 
