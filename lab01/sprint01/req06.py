943396586a4f26cf340ee8f28544d210cce823dc
HugoPoletto34 <hugopoletto34@gmail.com>
2024-02-28 21:00:08 -0300

    correção query req 06

--- lab01/sprint01/req06.py
+++ lab01/sprint01/req06.py
@@ -1,15 +1,18 @@
 from github_graph_ql import run_query
 
 
-def get_issues_closed(cursor=None, num_repos=100, query_type="stars:>0", type="REPOSITORY", pages=1):
+def get_repositories_req_6(cursor=None, num_repos=100, query_type="stars:>0", type="REPOSITORY", pages=1):
     query = """
     {
     search(query: "%s", type: %s, first: %s, after: %s) {
         nodes {
             ... on Repository {
                nameWithOwner
-               issues(states: [CLOSED]) {
-                    totalCount
+               issues(states: [OPEN] ) {
+                  totalCount
+                }
+                closedIssues: issues(states: [CLOSED]) {
+                  totalCount
                 }
             }
         }
@@ -22,33 +25,15 @@ def get_issues_closed(cursor=None, num_repos=100, query_type="stars:>0", type="R
 
     for repo_info in repositories:
         name_with_owner = repo_info['nameWithOwner']
-        closed_issues = repo_info['issues']['totalCount']
-        print(f"Repositório: {name_with_owner} - Total de issues fechadas: {closed_issues}")
-
-def get_issues_totais(cursor=None, num_repos=100, query_type="stars:>0", type="REPOSITORY", pages=1):
-    query = """
-    search(query: "%s", type: %s, first: %s, after: %s) {
-        nodes {
-            ... on Repository {
-               name
-               issues {
-                    totalCount
-                }
-            }
-        }
-      }
-    }
-    """ % (query_type, type, num_repos, f'"{cursor}"' if cursor else "null")
-
-    result = run_query(query)
-    repositories = result['data']['search']['edges']
-
-    for repo in repositories:
-        repo_info = repo['node']
-        name_with_owner = repo_info['nameWithOwner']
-        total_issues = repo_info['issues']['totalCount']
-        print(f"Repositório: {name_with_owner} - Issues totais: {total_issues}")
+        open_issues = repo_info['issues']['totalCount']
+        closed_issues = repo_info['closedIssues']['totalCount']
+        total_issues = (open_issues + closed_issues)
+        if total_issues == 0:
+            percentual = 0
+        else:
+            percentual = (closed_issues / total_issues) * 100
+        print(f"Repositório: {name_with_owner} - Percentual: {percentual}")
 
 
 if __name__ == '__main__':
-    get_issues_closed(num_repos=100, pages=1)
+    get_repositories_req_6(num_repos=100, pages=1)
