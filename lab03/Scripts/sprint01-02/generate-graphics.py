b60c2bfbd11c93d2e13f0023551bbe995390308f
HugoPoletto34 <hugopoletto34@gmail.com>
2024-04-21 10:16:04 -0300

    Correção do título do gráfico

--- lab03/Scripts/sprint01-02/generate-graphics.py
+++ lab03/Scripts/sprint01-02/generate-graphics.py
@@ -19,6 +19,6 @@ correlation_matrix = df.corr()
 # Plotando a matriz de correlação
 plt.figure(figsize=(15, 13), dpi=100, facecolor='w', edgecolor='k', frameon=True, clear=True, linewidth=1.0, tight_layout=True)
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", square=True)
-plt.title('Matriz de Correlação entre Métricas e Feedback Final das Revisões')
+plt.title('Matriz de Correlação entre Métricas de Pull Requests')
 plt.savefig('graphics/correlation_matrix.png')
 plt.show()
